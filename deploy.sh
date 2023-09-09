11316ec0455e7099330184c3552f8f67e094c75c
emma <orderhero.emma@gmail.com>
2023-09-09 23:15:59 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKER_APP_NAME=emma-test
+DOCKER_APP_NAME=emma-aws-test
 
 cd /home/ec2-user/emma-test
 
@@ -9,7 +9,7 @@ EXIST_CONTAINER=$(docker ps | grep $DOCKER_APP_NAME-main)
 if [ -z "$EXIST_CONTAINER" ]; then
 	docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml -d --build
 
-	echo "Build Complete senior-nutrition-main container"
+	echo "Build Complete emma-aws-test container"
 fi
 
 docker system prune -af

1f51c26e09a47816692cf2c9ea771db49c62e019
emma <orderhero.emma@gmail.com>
2023-09-13 13:55:32 +0900

    test

--- www/index.php
+++ www/index.php
@@ -1,20 +1,24 @@
 <?php
 echo "헬로월드";
+
 $db_host = "host=emma.cpf6y5ls20wt.ap-northeast-2.rds.amazonaws.com";
-$db_port ="port=5432";
-$dd_user = "user=postgres";
+$db_port = "port=5432";
+$db_user = "user=postgres";
 $db_password = "password=12345678";
 $db_name = "emma";
 
-$conn = pg_connect("$db_host $db_port $dd_user $db_password $db_name");
+// pg_connect() 함수는 연결 문자열을 하나의 문자열로 전달해야 합니다.
+$connection_string = "$db_host $db_port $db_user $db_password $db_name";
 
+$conn = pg_connect($connection_string);
 
 $query = "SELECT * FROM test";
 $result = pg_query($conn, $query);
+
 $db_result = "";
 
 while ($row = pg_fetch_assoc($result)) {
   $db_result = $row['test'];
 }
 
-echo "".$db_result;
+echo $db_result;
